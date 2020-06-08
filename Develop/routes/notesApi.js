ebd6589bdebada1897402e2d8343637a2366de7c
Brice Boutet <boutetlb@gmail.com>
2020-06-07 22:51:53 -0400

    Notes API - POST /api/notes to write and save a new note

--- Develop/routes/notesApi.js
+++ Develop/routes/notesApi.js
@@ -26,4 +26,28 @@ module.exports = function(app) {
         res.send(JSON.parse(notes_db));
     });
 
+    //Writing and saving a new note note to save
+    app.post("/api/notes", function(req, res) {
+
+        //Receive a new note to save on the request body
+        const title = req.body.title;
+        const text = req.body.text;
+
+        //Add the new note to the db.json file,
+        const notes_db = JSON.parse((fs.readFileSync("./db/db.json", function(error) {
+                if (error) throw error;
+            })))
+            // The note id is determined by incrementing the last note id
+        const lastNote = notes_db[notes_db.length - 1];
+        notes.push({
+            "title": title,
+            "text": text,
+        });
+        fs.writeFileSync("./db/db.json", JSON.stringify(notes_db), function(error) {
+                if (error) throw error;
+            })
+            //Return the new note to the client.
+        res.send(notes_db);
+    });
+
 };
